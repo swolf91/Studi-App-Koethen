81b8987d151fc003dccb89d093b7e6a0219b046c
Stefan Wolf <boeserwolf91@googlemail.com>
2013-04-25 21:01:48 +0200

    angefangen mit xml Parser Ressourcen zu laden

--- src/de/hsanhalt/inf/studiappkoethen/MainActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/MainActivity.java
@@ -1,9 +1,13 @@
 package de.hsanhalt.inf.studiappkoethen;
 
-import android.os.Bundle;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import de.hsanhalt.inf.studiappkoethen.utils.BuildingCategoryManager;
+import de.hsanhalt.inf.studiappkoethen.utils.XmlParser;
 
 public class MainActivity extends Activity
 {
@@ -14,15 +18,41 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-
-
-        BuildingCategoryManager buildingCategoryManager = BuildingCategoryManager.getInstance();
-        if(!buildingCategoryManager.hasContent())
+        if(!XmlParser.isCreated())
         {
-            buildingCategoryManager.loadXML(this.getResources().openRawResource(R.xml.categories));
+
+            XmlParser xmlParser = XmlParser.getInstance();
+            AssetManager assets = this.getAssets();
+
+            try
+            {
+                xmlParser.parse(assets.open("xml/categories.xml"));
+            }
+            catch (IOException e)
+            {
+                Log.e("FileException", "Can't parse file categories.xml from assets", e);
+            }
+
+            //xmlParser.parse(assets.open("xml/"));
+            //TODO alle xml-Dateien laden.
         }
-        buildingCategoryManager.getCategory(1);
-	}
+
+
+//        try
+//        {
+//            BuildingCategoryManager buildingCategoryManager = BuildingCategoryManager.getInstance();
+//            if (!buildingCategoryManager.hasContent())
+//            {
+//                buildingCategoryManager.loadXML(this.getAssets().open("xml/categories.xml"));
+//                //buildingCategoryManager.loadXML(this.getResources().openRawResource(R.assets.categories));
+//            }
+//            buildingCategoryManager.getCategory(1);
+//        }
+//        catch (Exception e)
+//        {
+//            Log.e("ResourceError", "Can't load the Resource: categories.xml", e);
+//        }
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
