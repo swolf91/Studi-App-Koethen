31b8c525e05c9e20fbf056d88ed3e774a9bd8dc4
Stefan Weichel <weichelstefan@online.de>
2013-05-11 14:57:20 +0200

    Implementierung GoogleMaps und Einfügen einer Tutorial-Textdatei zur Aktivierung von GoogleMaps

--- src/de/hsanhalt/inf/studiappkoethen/MainActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/MainActivity.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import de.hsanhalt.inf.studiappkoethen.util.xml.parsing.XmlParser;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+import android.app.FragmentManager;
+
 public class MainActivity extends Activity
 {
-
+	static final LatLng KOETHEN = new LatLng(51.750, 11.967);
+	private GoogleMap map;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -20,6 +30,17 @@ public class MainActivity extends Activity
             xmlParser.setAssets(this.getAssets());
             xmlParser.install();
         }
+        
+
+		FragmentManager myFragmentManager = getFragmentManager();
+        MapFragment myMapFragment 
+         = (MapFragment)myFragmentManager.findFragmentById(R.id.map);
+        map = myMapFragment.getMap();
+        map.setMyLocationEnabled(true);        
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(KOETHEN, 14));
+	    map.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
     }
 
     @Override
