15ac0924a97e381fba22cd8c7b42aa69da0c9b98
Thorsthen <torsten1337@hotmail.com>
2013-05-14 11:10:57 +0200

    to be done soon....

---
+++ src/de/hsanhalt/inf/studiappkoethen/util/xml/person/Person.java
@@ -0,0 +1,80 @@
+package de.hsanhalt.inf.studiappkoethen.util.xml.person;
+
+import java.net.URL;
+
+public class Person
+{
+	/**
+	 * beinhaltet den Namen der Person
+	 */
+	private final String name;
+	/**
+	 * beinhaltet den Vornamen der Person
+	 */
+	private final String surname;
+	/**
+	 * beinhaltet die aktuellen Personalstatus der Person (Prof., Mitarbeiter, usw.)
+	 */
+	private final String state;
+	/**
+	 * Fachgebiet der Person
+	 */
+	private final String specialField;
+	/**
+	 * Straße des Arbeitsortes einer Person
+	 */
+	private final String street;
+	/**
+	 * Straßenummer des Arbeitsortes einer Person
+	 */
+	private final String streetNumber;
+	/**
+	*	PLZ des Arbeitsortes einer Person
+	*/
+	private final String postalCode;
+	/**
+	* Stadt des Arbeitsortes einer Person
+	*/
+	private final String city;
+	/**
+	* Gebäude in dem die Person ihren Arbeitsort hat
+	*/
+	private final String buildings;
+	/**
+	* Büroraum der Person
+	*/
+	private final String room;
+	/**
+	 * Personenbeschreibung
+	 */
+	private final String description;
+	/**
+	 * Fach oder Arbeitsbereich der Person
+	 */
+	private final String profession;
+	/**
+	 * Module welche die Person unterhält
+	 */
+	private final String[] modul;
+	/**
+	 *  Für was die Person verantwortlich ist
+	 */
+	private final String[] responsibility;
+	/**
+	 * Sprechzeiten
+	 */
+	private final String talkTime;
+	/**
+	 * Telefonnummer der Person
+	 */
+	private final String phone;
+	/**
+	 * E-Mail der Person
+	 */
+	private final String email;
+	/**
+	 * Url auf die Website der Person
+	 */
+	private final String url;
+	
+}
