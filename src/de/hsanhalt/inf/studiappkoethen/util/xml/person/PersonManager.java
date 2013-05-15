8d47c2db4021587fcf4b6f7c3e902b198c1eb9ee
Thorsthen <torsten1337@hotmail.com>
2013-05-15 21:28:08 +0200

    addPerson() herausgenommen da addElement(xyz) vorhanden ist

--- src/de/hsanhalt/inf/studiappkoethen/util/xml/person/PersonManager.java
+++ src/de/hsanhalt/inf/studiappkoethen/util/xml/person/PersonManager.java
@@ -1,26 +1,30 @@
 package de.hsanhalt.inf.studiappkoethen.util.xml.person;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class PersonManager {
+import android.util.Log;
+import de.hsanhalt.inf.studiappkoethen.util.xml.parsing.IXmlParsing;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class PersonManager implements IXmlParsing{
 	
 	private static PersonManager INSTANCE;
+	private List<Person> person;
 	
 	
-	private Set<Person> person;
-	
-	
+/**
+ * Standartkonstruktor. Lädt alle in als XML-Daten vorhandenen Personen in den RAM.
+ */
 	private PersonManager(){
 		
-		person = new HashSet<Person>();
+		person = new ArrayList<Person>();
 		// TODO XML DateiEN laden und Personenobjekte erzeugen.
+		
 	}
 	
-	public void addPerson(Person newperson){
-		this.person.add(newperson);
-		// TODO Sortieren nach Kategorie....
-	}
 	
 	/**
 	 * Gibt eine Instanz dieser Klasse zurueck und sorgt dafuer, dass auch nur
@@ -36,6 +40,220 @@ public class PersonManager {
 		}
 		return INSTANCE;
 	}
+	
+	
+	/**
+	 * 
+	 * Gibt für 0..n Kategorien eine Personenliste zurück.
+	 * @param categories 0..n Möglichkeiten
+	 * @return Alle Personen der angeforderten Kategorieen
+	 */
+	public List<Person> getPersonList(PersonCategory... categories)
+    {
+        List<Person> personList;
+        if (categories == null || categories.length == 0)
+        {
+            personList = new ArrayList<Person>(this.person);
+        }
+        else
+        {
+            personList = new ArrayList<Person>();
+            List<PersonCategory> categoryList = Arrays.asList(categories);
+
+            for (Person person : this.person)
+            {
+                if (categoryList.contains(person.getPersonCategory()))
+                {
+                	personList.add(person);
+                }
+            }
+        }
+        return personList;
+    }
+
+	@Override
+	public void addNode(Node node)
+    {
+        if (!this.getStartTag().equals(node.getNodeName()))
+        {
+            return;
+        }
+        if (node.hasChildNodes())
+        {
+            NodeList nodes = node.getChildNodes();
+            PersonCategory category = null;
+
+            for (int i = 0; i < nodes.getLength(); i++)
+            {
+                Node subNode = nodes.item(i);
+                if (subNode.getNodeName().equals("categoryID"))
+                {
+                    category = PersonCategoryManager.getInstance()
+                                                      .getCategory(Byte.valueOf(subNode.getTextContent()));
+                }
+                else if (subNode.getNodeName().equals("person"))
+                {
+                    this.addElement(category, subNode);
+                }
+            }
+        }
+    }
+	/**
+	 * Elemente werden aus der XML ausgelesen und als Personenobjekte angelegt.
+	 * @param category wird benötigt um schneller auf die Person zu schließen.
+	 * @param node aktueller XML Knoten
+	 */
+	private void addElement(PersonCategory category, Node node)
+	{
+		 if (category == null)
+	        {
+	            Log.e("MatchPersonError", "Tried to match person without category.");
+	            return;
+	        }
+
+	        String name = null;
+			String surname = null;
+			String state = null;
+			String specialField = null;
+			String street = null;
+			String houseNumber = null;
+			String postalCode = null;
+			String city = null;
+			String buildings = null;
+			String room = null;
+			String description = null;
+			String profession = null;
+			String []module = null;
+			String []responsibility = null;
+			String talkTime = null;
+			String phone = null;
+			String email = null;
+			String url = null;
+	        
+
+
+	        NodeList list = node.getChildNodes();
+	        for (int i = 0; i < list.getLength(); i++)
+	        {
+	            Node subNode = list.item(i);
+	            String nodeName = subNode.getNodeName();
+	            String content = subNode.getTextContent();
+
+	            if (nodeName.equals("name"))
+	            {
+	                name = content;
+	            }
+	            else if (nodeName.equals("surname"))
+	            {
+	                surname = content;
+	            }
+	            else if (nodeName.equals("state"))
+	            {
+	                state = content;
+	            }
+	            else if (nodeName.equals("specialField"))
+	            {
+	                specialField = content;
+	            }
+	            else if (nodeName.equals("street"))
+	            {
+	                street = content;
+	            }
+	            else if (nodeName.equals("houseNumber"))
+	            {
+	                houseNumber = content;
+	            }
+	            else if (nodeName.equals("postalCode"))
+	            {
+	                postalCode = content;
+	            }
+	            else if (nodeName.equals("city"))
+	            {
+	                city = content;
+	            }
+	            else if (nodeName.equals("buildings"))
+	            {
+	                buildings = content;
+	            }
+	            else if (nodeName.equals("room"))
+	            {
+	                room = content;
+	            }
+	            else if (nodeName.equals("description"))
+	            {
+	                description = content;
+	            }
+	            else if (nodeName.equals("profession"))
+	            {
+	                profession = content;
+	            }
+	            else if (nodeName.equals("street"))
+	            {
+	                street = content;
+	            }
+	            else if (nodeName.equals("module"))
+	            {
+	            	
+	            	NodeList childList= subNode.getChildNodes();
+	            	module= new String[childList.getLength()]; 
+	            	
+	            	for(int k=0; k<childList.getLength();k++){
+	            		module[i] = childList.item(k).getNodeValue();
+	            	}
+	            	
+	            }
+	            else if (nodeName.equals("responsibility"))
+	            {
+	            	
+	            	NodeList childList= subNode.getChildNodes();
+	            	responsibility= new String[childList.getLength()];
+	            	
+	            	for(int k=0; k<childList.getLength();k++){
+	            		responsibility[i]  = childList.item(k).getNodeValue();
+	            		
+	            	}
+	            	
+	            }
+	            else if (nodeName.equals("talkTime"))
+	            {
+	                talkTime = content;
+	            }
+	            else if (nodeName.equals("phone"))
+	            {
+	                phone = content;
+	            }
+	            else if (nodeName.equals("email"))
+	            {
+	                email = content;
+	            }
+	            else if (nodeName.equals("url"))
+	            {
+	                url = content;
+	            }
+	        }
+
+	        Person newPerson;
+
+	        newPerson = new Person(category, name, surname,state,specialField,street, houseNumber, 
+			postalCode, city, buildings, room, description, profession,
+			module,responsibility,talkTime,phone,email,url);
+	        
+	        this.person.add(newPerson);
+
+	        Log.d("Created " + person.getClass().getSimpleName(), category.getName() + " - " + name);
+	    
+		
+	}
+	/**
+	 * Sucht den Anfang der Person XML.
+	 * @return String 
+	 */
+	@Override
+	public String getStartTag()
+	{
+		return "person";
+	}
+
 	
 	
 
