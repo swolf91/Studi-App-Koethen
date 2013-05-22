3442ec8a4c9b4665950241c062c30ee3a0049679
Steven Piotter <Sutii@hotmail.de>
2013-05-22 11:28:16 +0200

    Class für die EntdeckertourActivity eingebaut

---
+++ src/de/hsanhalt/inf/studiappkoethen/activities/EntdeckertourActivity.java
@@ -0,0 +1,16 @@
+package de.hsanhalt.inf.studiappkoethen.activities;
+
+import de.hsanhalt.inf.studiappkoethen.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntdeckertourActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_entdeckertour);
+    }
+}
