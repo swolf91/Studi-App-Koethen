fe853f47bee97ec517eb5e44cb331b759a004758
Steven Piotter <Sutii@hotmail.de>
2013-05-22 12:48:36 +0200

    einladen des Layouts

--- src/de/hsanhalt/inf/studiappkoethen/activities/KoethenActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/KoethenActivity.java
@@ -1,6 +1,16 @@
 package de.hsanhalt.inf.studiappkoethen.activities;
 
-public class KoethenActivity
-{
+import de.hsanhalt.inf.studiappkoethen.R;
+import android.app.Activity;
+import android.os.Bundle;
 
+public class KoethenActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_koethen);
+    }
 }
