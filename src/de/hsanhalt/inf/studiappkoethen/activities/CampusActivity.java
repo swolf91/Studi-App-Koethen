844e70268098f36358e6d9da06ed3205f38c55bc
Steven Piotter <Sutii@hotmail.de>
2013-05-22 11:28:02 +0200

    Class für die CampusActivity eingebaut

---
+++ src/de/hsanhalt/inf/studiappkoethen/activities/CampusActivity.java
@@ -0,0 +1,16 @@
+package de.hsanhalt.inf.studiappkoethen.activities;
+
+import de.hsanhalt.inf.studiappkoethen.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CampusActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_campus);
+    }
+}
