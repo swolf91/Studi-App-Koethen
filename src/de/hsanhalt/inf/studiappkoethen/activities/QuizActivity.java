0d7c9d9c0b345d4a3d9f35b36cdeb42f26c010a8
Nils Anderson <NA-SA@gmx.de>
2013-05-29 18:10:21 +0200

    Optionsmenü

--- src/de/hsanhalt/inf/studiappkoethen/activities/QuizActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/QuizActivity.java
@@ -1,5 +1,8 @@
 package de.hsanhalt.inf.studiappkoethen.activities;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import de.hsanhalt.inf.studiappkoethen.R;
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,5 +15,40 @@ public class QuizActivity extends Activity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_quiz);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+        case R.id.action_close:
+            moveTaskToBack(true);
+            return true;
+
+        case R.id.action_main:
+            Intent intent  = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+
+//        case R.id.action_list:
+//            startActivity(new Intent(this, ExpandableListActivity.class));
+//            return true;
+
+
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
     }
 }
