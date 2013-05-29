a4698aed02cbd91e5c1b50c23043e250d5b54b01
Thorsthen <torsten1337@hotmail.com>
2013-05-29 22:57:20 +0200

    +neue Methode mit Log

--- src/de/hsanhalt/inf/studiappkoethen/util/ExpandableListViewAdapter.java
+++ src/de/hsanhalt/inf/studiappkoethen/util/ExpandableListViewAdapter.java
@@ -8,7 +8,9 @@ import de.hsanhalt.inf.studiappkoethen.util.xml.buildings.Building;
 import de.hsanhalt.inf.studiappkoethen.util.xml.buildings.BuildingCategory;
 import de.hsanhalt.inf.studiappkoethen.util.xml.buildings.BuildingCategoryManager;
 import de.hsanhalt.inf.studiappkoethen.util.xml.buildings.BuildingManager;
+import android.app.ExpandableListActivity;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
@@ -22,57 +24,59 @@ import android.widget.ListAdapter;
 public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 	private Context context;
 
-	BuildingCategoryManager bc;
-	int size=bc.getInstance().getSize();
-
-	String[] categoryEntries=null;
-
-
-	BuildingManager bmanager;
+	BuildingCategoryManager bc;//Zugriff auf Kategorien
+	int size=bc.getInstance().getSize(); //Anzahl aller Kategorien (4 oder 3.. kp weiss ich atm nich lol)
+	String[] categoryEntries=new String[size];
+	BuildingManager bmanager; // Zugriff auf Gebaeude
 	List<Building> buildingList= new ArrayList<Building>();
 
 
 	/**
-	 * L�dt alle Gebaeude, nach Kategorie geordnet, in die ExpandableList
+	 * Laedt alle Gebaeude, nach Kategorie geordnet, in die ExpandableList
 	 * @param newcontext
 	 */
-	//TODO Baustelle.................
+	//TODO Parameter!
 	public ExpandableListViewAdapter(Context newcontext){
 
 		 this.context=newcontext;
 
-		 buildingList=bmanager.getInstance().getBuildingList(bc.getInstance().getBuildingCategories());
-
-		 BuildingCategory []category = new BuildingCategory[size];
-		 category=bc.getBuildingCategories();
-
-		 int index=size;
-		 while(buildingList.iterator().hasNext()){
-
-			 Building it=buildingList.iterator().next();
-
-
-
-				 for(int i=0;i<size;i++){
-					 if(it.getBuildingCategory().equals(category[i]))
-						 //Wenn das aktuelle Gebaeude einer Kategorie zugeordnet wurde
-						 //(sollte immer klappen - ausser ein Gebaeude hat keine Kategorie)
-
-					 categoryEntries[i]=it.getBuildingCategory().getName(); //2 Dimensionales Array?! TODO!!
-					 //Fuege das Gebaeude der richtigen Kategorie zu
-
-
-			 }
-
+		 BuildingCategory []category = new BuildingCategory[size]; // hier sind alle Kategorien drin
+		 category=bc.getInstance().getBuildingCategories();
+		 String [] parentEntries = new String[size];
+		 String [][] childEntries= new String[size][];
+		
+		 for(int i=0;i<size;i++){
+			 
+			 parentEntries[i]=childEntries[i][0]=category[i].getName();
+			 List<Building> tmp= new ArrayList<Building>();
+			 tmp=bmanager.getInstance().getBuildingList(category[i]);
+			 childEntries[i]= new String[tmp.size()];
+			 
+			 for(int k=0;k<tmp.size();i++)
+				 childEntries[i][k]=tmp.remove(0).getName();
+			 
 		 }
+		 
+		 Log.d("ExtpandableListView","Created parentEntries : " + parentEntries.length + "and childEntries : "+childEntries.length );
+		 
+		 
+//		 while(buildingList.iterator().hasNext()){
+//
+//			 Building it=buildingList.iterator().next();
+//
+//				 for(int i=0;i<size;i++){
+//					 if(it.getBuildingCategory().equals(category[i]))
+//						 //Wenn das aktuelle Gebaeude einer Kategorie zugeordnet wurde
+//						 //(sollte immer klappen - ausser ein Gebaeude hat keine Kategorie)
+//
+//					 categoryEntries.add(it.getBuilding());	 //Fuege das Gebaeude der richtigen Kategorie zu
+//					 
+//
+//				 }
+//
+//		 }
 
-		 categoryEntries=new String[size];
-
-
-		// categoryEntries=bc.getInstance().getAllBuildingCategoriesbyString();
-
-	}
-
+ 	}
 
 
 	@Override
