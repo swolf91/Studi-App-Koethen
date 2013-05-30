3e2b4846c4044d9022ab93db7107dbc9407c1d6c
Go <torsten1337@hotmail.com>
2013-05-30 12:28:34 +0200

    +more code yay!

--- src/de/hsanhalt/inf/studiappkoethen/activities/ExpandableListActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/ExpandableListActivity.java
@@ -1,27 +1,107 @@
 package de.hsanhalt.inf.studiappkoethen.activities;
 
 import android.content.Intent;
+//import android.support.v4.widget.SearchViewCompatIcs.MySearchView;
 import android.view.MenuItem;
 import de.hsanhalt.inf.studiappkoethen.R;
 import de.hsanhalt.inf.studiappkoethen.util.ExpandableListViewAdapter;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
 
 public class ExpandableListActivity extends android.app.ExpandableListActivity
 {
 	
-	ExpandableListView elv;
+	private ExpandableListView expandableListView;
+	private ExpandableListAdapter expandableListViewAdapter;
+	/**
+	 * diese Methode baut de ExpandableList auf und zeigt sie an
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_expandablelist);
 		
-		elv=(ExpandableListView)findViewById(R.id.expandableListView1);
-		//elv.setAdapter(new ExpandableListView(this)); //TODO
+		//Spinner ist zur "Vorschau" der Gruppenelemente
+		Spinner spinner = (Spinner) findViewById(R.id.expandableListView1);
+		
+		//ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R. , android.R.layout.simple_spinner_dropdown_item); TODO "Spinner" - Layout einfuergen
+		
+		//Layout das genutzt werden soll festlegen, wenn die Auswahlliste angezeigt wird
+//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		//den Adapter zum Spinner hinzufuegen
+//		spinner.setAdapter(adapter);
+		
+		
+		//weitere Daten laden also die ganzen Parent und Child Dinger
+		
+		//referenz auf unsere Exp Liste holen
+		expandableListView = (ExpandableListView) findViewById(R.id.expandableListView1);
+		
+		expandableListViewAdapter = new ExpandableListViewAdapter(ExpandableListActivity.this);
+		
+		expandableListView.setAdapter(expandableListViewAdapter);
+		
+		expandAll(); //TODO nur eins Expandieren lassen
+		
+		
+//		Button add = (Button) findViewById(R.id.) // hier muss ein Button hin
+				
+			
+		//listener fuer die Child Liste
+//		expandableListView.setOnChildClickListener()		
+		
+		
+		
+		
+		
 	}
+	/**
+	 * Alle Gruppen werden auf OnClick geoeffnet.
+	 */
+	private void expandAll()
+	{
+		int count = expandableListViewAdapter.getGroupCount();
+		for(int i=0;i<count;i++){
+			expandableListView.expandGroup(i);
+		}
+		
+	}
+	/**
+	 * Ereignis das bei "OnClick" auf das Untermenue der ExpandableList ausgeloest wird.
+	 */
+	private OnChildClickListener expandableListItemClicked = new OnChildClickListener () {
+		public boolean onChildClick(ExpandableListView parent, View view,int groupPosition,int childPosition,long id){
+			
+			//TODO hier kommt der aufruf zur Detail Activity des jeweiligen Gebäudes hin
+			return false;
+		}
+	};
+	/**
+	 * Ereignis das bei "OnOlick" auf die Kategorieelemente der ExpandableList ausgeloest wird.
+	 */
+	private OnGroupClickListener expandableListGroupClicked = new OnGroupClickListener () {
+		
+		public boolean onGroupClick(ExpandableListView parent, View view, int groupPosition, long id) {
+			
+			//TODO wenn die oberen Gruppenelemente gekickt sollen nur die jeweiligen Child Elemente aufgerufen werden.
+			return false;
+		}
+	};
+	
+	
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
@@ -46,7 +126,7 @@ public class ExpandableListActivity extends android.app.ExpandableListActivity
             startActivity(intent);
             return true;
 
-//        case R.id.action_list:
+//        case R.id.action_list:			//TODO rausnehmen wenn Liste fertig ist
 //            startActivity(new Intent(this, ExpandableListActivity.class));
 //            return true;
 
@@ -55,6 +135,14 @@ public class ExpandableListActivity extends android.app.ExpandableListActivity
             return super.onOptionsItemSelected(item);
         }
 
+    }
+    
+    public void onClick(View v){
+    	
+    	switch(v.getId()){
+    		
+    	}
+    	
     }
 
 
