b2033b10b0d144715577efc4a37bd32047bcb3ed
Stefan Wolf <boeserwolf91@googlemail.com>
2013-06-06 12:58:17 +0200

    quizActivity

--- src/de/hsanhalt/inf/studiappkoethen/activities/QuizActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/QuizActivity.java
@@ -1,13 +1,20 @@
 package de.hsanhalt.inf.studiappkoethen.activities;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import de.hsanhalt.inf.studiappkoethen.R;
@@ -19,8 +26,8 @@ import de.hsanhalt.inf.studiappkoethen.xml.quiz.QuizManager;
 
 public class QuizActivity extends Activity
 {
+    private List<Boolean> answers;
     SharedPreferences quizPreferences ;    // Gibt die id des zuletzt geloesten Quizes zurueck.
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,7 +36,7 @@ public class QuizActivity extends Activity
 
         this.setContentView(R.layout.activity_quiz);
         quizPreferences = getSharedPreferences("Quiz Status", MODE_PRIVATE);
-        int lastQuiz = quizPreferences.getInt("lastQuiz", -1);
+        byte lastQuiz = (byte) quizPreferences.getInt("lastQuiz", -1);
 
         if(lastQuiz != -1)
         {
@@ -38,11 +45,34 @@ public class QuizActivity extends Activity
             TextView textView = (TextView) this.findViewById(id.quiz_textView_noquiz);
             textView.setVisibility(View.GONE);
 
+            // TODO status laden!
+            this.load(lastQuiz);
+
             textView = (TextView) this.findViewById(id.quiz_textView_question_headline);
             textView.setVisibility(View.VISIBLE);
 
             textView = (TextView) this.findViewById(id.quiz_textView_question);
             textView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void load(byte quizID)
+    {
+        Quiz quiz = QuizManager.getInstance().getQuiz(quizID);
+        this.answers = new ArrayList<Boolean>(quiz.getNumberOfQuestions());
+        try
+        {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(this.openFileInput("quiz" + quizID));
+
+            bufferedInputStream.close();
+        }
+        catch (FileNotFoundException ignored)
+        {
+
+        }
+        catch (IOException e)
+        {
+            Log.e("QuizActivityError", "Can't read FileInputStream from quiz" + quizID + "!", e);
         }
     }
 
