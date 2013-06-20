33b411d4775df13d8783bf3113d58bac2a82be90
Go <torsten1337@hotmail.com>
2013-06-20 13:07:26 +0200

    +rest fertsch

--- src/de/hsanhalt/inf/studiappkoethen/activities/ExpandableListActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/ExpandableListActivity.java
@@ -44,12 +44,7 @@ public class ExpandableListActivity extends Activity
     SharedPreferences expListPreferences;
     boolean showBuildings;
     
-    
-//    final ImageView personButton = (ImageView) findViewById(R.id.imageView_Persons);
-//    final ImageView buildingButton = (ImageView) findViewById(R.id.imageView_Buildings);
-    
-    /** TODO engaging test...
-     * 
+    /**  
      * Initialaufbau der ExpandableList Activity.Gibt der Activity den Adapter und den Listener (jeweils fuer Personen und Gebaeude Objekte).
      */
 	@Override
@@ -85,7 +80,8 @@ public class ExpandableListActivity extends Activity
 				
 		}
 		else{
-
+			
+			this.showBuildings=true;
 			this.setContentView(R.layout.activity_expandablelist_koethen);
 	        this.expandableListView = (ExpandableListView) findViewById(R.id.expandableListView_Koethen);
 	        this.expandableListAdapterBuilding = new ExpandableListAdapter<BuildingCategory, Building>(this, this.getBuildingList(isCampus)); 
@@ -94,15 +90,6 @@ public class ExpandableListActivity extends Activity
 		}
 		
 		this.expandableListView.setOnChildClickListener(myListItemClicked);
-		
-//		this.personButton.setOnTouchListener(myOnTouchListenerPerson);
-//		this.buildingButton.setOnTouchListener(myOnTouchListenerBuilding);
-		
-
-		// wenn auf Koethen dann
-		// expandable list laedt alle geb ausser campus-geb
-		// sonst exp list bekommt nur campus bilder
-		
 
     }
 	
@@ -127,33 +114,6 @@ public class ExpandableListActivity extends Activity
 		}
 	}
 	
-	
-//	@Override
-//	protected void ()
-//	{
-//		// TODO Auto-generated method stub
-//		super.onResume();
-//		
-//		
-//		if(this.expListPreferences.getBoolean("lastBuildingPushState", true)){
-//			this.expandableListView.setAdapter(this.expandableListAdapterBuilding);
-//			this.expandableListView.setOnChildClickListener(myListItemClicked);
-//			//wenn geb dann lock geb button unlock person button...
-//			//change layouts
-//			//rufe geb auf
-//			this.expListPreferences.edit().putBoolean("lastBuildingPushState", true);
-//		}
-//		else{
-//			this.expandableListView.setAdapter(this.expandableListAdapterPerson);
-//			this.expandableListView.setOnChildClickListener(myListItemClicked);
-//			//wenn person dann lock person unlock geb button
-//			//change layouts
-//			//rufe person auf
-//			//setze lastBstate auf false
-//			
-//		}
-//	}
-
 
 
 	/**
@@ -257,76 +217,19 @@ public class ExpandableListActivity extends Activity
     {
         public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id)
         {
-            Intent intent = new Intent(getBaseContext(), DetailActivity.class);
-            intent.putExtra("category", expandableListAdapterBuilding.getGroup(groupPosition).getID());
-            intent.putExtra("building", expandableListAdapterBuilding.getChild(groupPosition, childPosition).getID());
-            startActivity(intent);
+        	if(showBuildings){
+        		Intent intent = new Intent(getBaseContext(), DetailActivity.class);
+                intent.putExtra("category", expandableListAdapterBuilding.getGroup(groupPosition).getID());
+                intent.putExtra("building", expandableListAdapterBuilding.getChild(groupPosition, childPosition).getID());
+                startActivity(intent);
+        	}else{
+        		//TODO Personen laden
+        	}
+            
             return true;
         }
     };
-    
-//    private OnTouchListener myOnTouchListenerPerson = new OnTouchListener()
-//	{
-//		
-//		public boolean onTouch(View view, MotionEvent motionEvent)
-//		{
-//			
-//			expListPreferences.edit().putBoolean("LastBuildingState", false);
-//				
-//			switch (motionEvent.getAction()){
-//						
-//				case MotionEvent.ACTION_DOWN:{
-//							
-//				view.setImageResource(R.drawable.geb_el); //TODO Buttonbilder sind noch zu editieren;
-//
-//				// selektierbild wird geladen
-//				// Gebaeude wird ausgegraut
-//				break;
-//				}
-//				case MotionEvent.ACTION_CANCEL:{
-////				view.setImageBitmap(res.getDrawable(R.drawable.icon_map_focus))); //TODO hier soll das default icon geladen werden
-//				break;
-//				}
-//			}
-//			
-//			return true;
-//		}
-//		
-//	};
-//	
-//	private OnTouchListener myOnTouchListenerBuilding = new OnTouchListener()
-//	{
-//		
-//		public boolean onTouch(View view, MotionEvent motionEvent)
-//		{
-//			
-//			expListPreferences.edit().putBoolean("LastBuildingState", true);
-//				
-//			switch (motionEvent.getAction()){
-//						
-//				case MotionEvent.ACTION_DOWN:{
-//							
-//				view.setImageBitmap(res.getDrawable(R.drawable.geb_el2)); //TODO Buttonbilder sind noch zu editieren;
-//
-//				// selektierbild wird geladen
-//				// Gebaeude wird ausgegraut
-//				break;
-//				}
-//				case MotionEvent.ACTION_CANCEL:{
-////				view.setImageBitmap(res.getDrawable(R.drawable.icon_map_focus))); //TODO hier soll das default icon geladen werden
-//				break;
-//				}
-//			}
-//			
-//			return true;
-//		}
-//		
-//	};
-
-
-	
-	
-    
+        
 
 
 }
