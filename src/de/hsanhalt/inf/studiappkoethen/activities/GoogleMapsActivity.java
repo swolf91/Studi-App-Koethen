f81aa2dbf0509ee8667d69faa8ee8d19edd0676d
Stefan Weichel <weichelstefan@online.de>
2013-06-24 11:59:54 +0200

    Programmcode aufgeräumt

--- src/de/hsanhalt/inf/studiappkoethen/activities/GoogleMapsActivity.java
+++ src/de/hsanhalt/inf/studiappkoethen/activities/GoogleMapsActivity.java
@@ -13,7 +13,6 @@ import de.hsanhalt.inf.studiappkoethen.xml.buildings.BuildingCategory;
 import de.hsanhalt.inf.studiappkoethen.xml.buildings.BuildingCategoryManager;
 import de.hsanhalt.inf.studiappkoethen.xml.buildings.BuildingManager;
 import de.hsanhalt.inf.studiappkoethen.util.FilterItem;
-import de.hsanhalt.inf.studiappkoethen.util.MergedMarkers;
 import de.hsanhalt.inf.studiappkoethen.util.ExtendetMarker;
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -30,12 +29,10 @@ import android.view.View;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapFragment;
 
@@ -44,15 +41,12 @@ public class GoogleMapsActivity extends Activity
 	static final LatLng KOETHEN = new LatLng(51.750, 11.967);			// Standardkoordinaten fuer den Kamerastandpunkt
 	static final float startZoomLevel = 14.0f;							// Start-Zoomlevel
 	private GoogleMap map;
-	private float currentZoomLevel = startZoomLevel;					// momentanes Zoomlevel fuer den OnCameraCangeListener
-	private float previousZoomLevel = 1.0f;								// vorheriges Zoomlevel fuer den OnCameraCangeListener
 	private double averageLatitude = 0.0f;								// Variable zur Durchschnittsberechnung des Breitengrades
 	private double averageLongitude = 0.0f;								// Variable zur Durchschnittsberechnung des Laengengrades
 	private int numberFilterItems = 0;									// Variable zur Durchschnittsberechnung des Breiten- und Laengengrades
 	private List<Byte> Categories = new ArrayList<Byte>();
-	private List<FilterItem> specialFilter = new ArrayList<FilterItem>();	// angepasster Filter der anhand der �ueergebenen Daten gef�uelt wird
+	private List<FilterItem> specialFilter = new ArrayList<FilterItem>();	// angepasster Filter der anhand der uebergebenen Daten gefuellt wird
 	private List<ExtendetMarker> displayedMarkers = new ArrayList<ExtendetMarker>();
-	private List<MergedMarkers> mergedMarkerList = new ArrayList<MergedMarkers>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -65,8 +59,6 @@ public class GoogleMapsActivity extends Activity
         MapFragment myMapFragment = (MapFragment)myFragmentManager.findFragmentById(R.id.map);
         map = myMapFragment.getMap();
 	    
-	    map.setOnCameraChangeListener(getCameraChangeListener());
-        
 	    map.setMyLocationEnabled(true);		// Aktivieren der Anzeige des eigenen Standortes
 	    
 	    getCategories();	// Ermitteln der Kategorie-Anzahl
@@ -106,9 +98,7 @@ public class GoogleMapsActivity extends Activity
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)	// Erstellung des Filter-Kontextmenues
 	{
 		super.onCreateContextMenu(menu, v, menuInfo);
-
 		BuildingCategoryManager bcm = BuildingCategoryManager.getInstance();
-		
 		menu.setHeaderTitle("Filteroptionen");
 		for(byte category : Categories) {				// Erstellt fuer jede vorhandene Kategorie eine Filter-Checkbox
 			String str = "";
@@ -117,10 +107,8 @@ public class GoogleMapsActivity extends Activity
 					str = buildingCategory.getName();
 				}
 			}
-			
 			MenuItem item = menu.add(0, category, 0, str);
 			item.setCheckable(true);
-			
 			for(FilterItem fItem : specialFilter) {		// Bestimmt, ob eine Checkbox true oder false ist
 				if(fItem.getCategory() == category) {
 					item.setChecked(true);
@@ -142,25 +130,20 @@ public class GoogleMapsActivity extends Activity
 		if(!filterOn) {
 			specialFilter.add(new FilterItem((byte)item.getItemId())); // ... wenn nicht, erstelle einen.
 		}
-		
 		item.setChecked(!item.isChecked());						// Aktivierung bzw. Deaktivierung der Checkbox
-		
 		clearAllMarkers();										// Ruecksetzen und Neuzeichnen aller Marker (wird noch optimiert)
     	setMarkersDependingOnFilter();
 		return super.onContextItemSelected(item);
 	}
 
 	private void getCategories() {							// Erstellt eine Liste aller verwendeten Kategorien
-
 		BuildingCategoryManager bcm = BuildingCategoryManager.getInstance();
-		
 		for(BuildingCategory category : bcm.getBuildingCategories())
 		{
 			if(!Categories.contains(category.getID())) {
 				Categories.add(category.getID());
 			}
 		}
-		
 	}
 	
 	public BitmapDescriptor getCategoryIcon(byte category) {
@@ -169,21 +152,17 @@ public class GoogleMapsActivity extends Activity
         BitmapDescriptor bitmapDescriptor = null;
 		String path = buildingCategoryManager.getCategory(category).getIconPath();
 
-		if(path != null)
-        {
-            try
-            {
+		if(path != null) {
+            try {
                 Bitmap bitmap = AndroidUtils.getBitmapFromAsset(this.getAssets(), path);
                 bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
             }
-            catch (IOException e)
-            {
+            catch (IOException e) {
                 bitmapDescriptor = null;
                 Log.e("GoogleMapsActivity", "Can't load category icon " + category + "!");
             }
 		}
-        if(bitmapDescriptor == null)     // Festlegen eines Default-Bildchens
-        {
+        if(bitmapDescriptor == null) {     // Festlegen eines Default-Bildchens
             bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher);
 		}
         return bitmapDescriptor;
@@ -278,18 +257,15 @@ public class GoogleMapsActivity extends Activity
 					.icon(getCategoryIcon(index))
 					.snippet(building.getDescription()));
 		}
-		
 		map.setOnInfoWindowClickListener(			// Setzen der OnClickListener fuer das Infofenster des erstellten Markers
-				new OnInfoWindowClickListener() {
-					@Override
-					public void onInfoWindowClick(Marker clickedMarker) {
-						startDetailedView(clickedMarker.getId());		// Starten der Detail-Ansicht des Gebaeudes
-                    }
+			new OnInfoWindowClickListener() {
+				@Override
+				public void onInfoWindowClick(Marker clickedMarker) {
+					startDetailedView(clickedMarker.getId());		// Starten der Detail-Ansicht des Gebaeudes
                 }
-            );
-
+            }
+        );
         displayedMarkers.add(new ExtendetMarker(newBuildingMarker, building.getBuildingCategory().getID(), building.getID()));		// Speichern aller angezeigten Markers, so das sie von anderen Funktionen leichter abgefragt werden koennen.
-		
 	}
 	
 	public void setMarkersDependingOnFilter(){		//Setzen aller Marker
@@ -325,19 +301,12 @@ public class GoogleMapsActivity extends Activity
 		}
 		if(matchedMarkerFound) {
 			Intent intent = new Intent(this, DetailActivity.class);				// Erstellung eines Intents fuer die Detail-Ansicht.
-	        intent.putExtra("category", currentCategId);						// �bergabe der Kategorie und Gebaeude-ID.
+	        intent.putExtra("category", currentCategId);						// Uebergabe der Kategorie und Gebaeude-ID.
 	        intent.putExtra("building", currentBuildId);
 	        this.startActivity(intent);											// Aufruf der Detail-Ansicht.
 		}
 	}
 	
-	public void clearMarker(int i) {				// Loeschen eines spezifischen Markers
-		if(displayedMarkers.size() > i) {
-			Marker delMkr = displayedMarkers.get(i).getMarker();
-			displayedMarkers.remove(i);
-			delMkr.remove();
-		}
-	}
 	
 	public void clearAllMarkers() {				// Loeschen aller dargestellten Marker
 		while(displayedMarkers.isEmpty() == false) {
@@ -346,104 +315,4 @@ public class GoogleMapsActivity extends Activity
 			first.remove();
 		}
 	}
-	
-	public int getZoomTriggerDistance() {
-		// ...still in Progress
-		float tmp = (16.4f - currentZoomLevel) * 500;
-		if(tmp > 0) {
-			return (int) tmp;
-		}else {
-			return 0;
-		}
-	}
-	
-	public void mergeCloseMarkers() {
-		// ...still in Progress
-		int triggerDistance = getZoomTriggerDistance();
-		//List<Marker> mergedMarkers = new ArrayList<Marker>();
-		
-		boolean stillMoreMergableMarkers = true;
-		
-		while(stillMoreMergableMarkers) {
-			int i = 0;
-			int j = 1;
-			boolean mergableMarkersFound = false;
-			if(displayedMarkers.size() > 1) {
-				while((i < displayedMarkers.size() - 1) && (!mergableMarkersFound)) {
-					while((j < displayedMarkers.size()) && (!mergableMarkersFound)) {
-						double dLat = (displayedMarkers.get(i).getMarker().getPosition().latitude - displayedMarkers.get(j).getMarker().getPosition().latitude) * 10000000;
-						double dLon = (displayedMarkers.get(i).getMarker().getPosition().longitude - displayedMarkers.get(j).getMarker().getPosition().longitude) * 10000000;
-						double dTotal = Math.sqrt(dLat * dLat + dLon * dLon);
-						
-						if((dTotal < triggerDistance) && (dTotal > 0.0)) {
-							mergableMarkersFound = true;
-							
-							Log.w("MARKERS FOUND", displayedMarkers.get(i).getMarker().getTitle() + " <-> " + displayedMarkers.get(j).getMarker().getTitle() + " = " + dTotal);
-						}
-						j++;
-					}
-					i++;
-				}
-			} else {
-				stillMoreMergableMarkers = false;
-			}
-			if(mergableMarkersFound) {
-				Marker m1 = displayedMarkers.get(i - 1).getMarker();
-				Marker m2 = displayedMarkers.get(j - 1).getMarker();
-				MergedMarkers newMerge;// = new MergedMarkers(m1, m2);// = new MergedMarkers(displayedMarkers.get(i - 1), displayedMarkers.get(j - 1));
-				
-				//mergedMarkerList.add(newMerge);	
-				
-				clearMarker(i - 1);	
-				clearMarker(j - 1);
-			} else {
-				stillMoreMergableMarkers = false;
-			}
-		}
-		/*
-		for(int i = 0; i < mergedMarkerList.size(); i++) {
-			Marker newBuildingMarker = map.addMarker(mergedMarkerList.get(i).getMarkerOptions());
-			displayedMarkers.add(newBuildingMarker);
-		}/**/
-	}
-	
-	public void splitMergedMarkers() {
-		// ...still in Progress
-		int triggerDistance = getZoomTriggerDistance();
-		// TODO: Funktion zum Splitten gemergeter Marker
-	}
-	
-	public OnCameraChangeListener getCameraChangeListener() {
-		// ...still in Progress
-		return new OnCameraChangeListener() {
-			@Override
-			public void onCameraChange(CameraPosition position) {
-				if(previousZoomLevel != position.zoom){
-				
-					currentZoomLevel = position.zoom;
-					
-					StringBuilder sbld = new StringBuilder();
-					String str = "";
-					sbld.append(currentZoomLevel);
-					str = sbld.toString();
-					
-					if(currentZoomLevel <= 11.0f) {
-						//clearAllMarkers();
-					}
-										
-					if(previousZoomLevel < currentZoomLevel) {
-						mergeCloseMarkers();		// Kombinieren von nahen Markern
-						
-					} else {
-						splitMergedMarkers();		// Teilen von kombinierten Markern
-					}
-					
-					previousZoomLevel = currentZoomLevel;
-				}
-			}
-		};
-	}
-	
-	
-	
 }
--- src/de/hsanhalt/inf/studiappkoethen/util/ExtendetMarker.java
+++ src/de/hsanhalt/inf/studiappkoethen/util/ExtendetMarker.java
@@ -3,7 +3,10 @@ package de.hsanhalt.inf.studiappkoethen.util;
 import com.google.android.gms.maps.model.Marker;
 
 /*
- * ...still in Progress 
+ * Da ein Marker keine Zusatzdaten speichern kann, werden durch diese Klasse noch die ID's 
+ * der Kategorie und des Gebaeudes zu dem Marker gespeichert.
+ * Eine Aenderung der Daten nach der Initialisierung ist nicht vorgesehen, daher gibt es 
+ * nur Getter- und keine Setter-Funktionen.
  */
 
 public class ExtendetMarker
@@ -18,15 +21,15 @@ public class ExtendetMarker
 		buildingId = bldn;
 	}
 	
-	public Marker getMarker() {
+	public Marker getMarker() {		// Rueckgabe des Markers
 		return eMarker;
 	}
 	
-	public byte getCategId() {
+	public byte getCategId() {		// Rueckgabe der Kategorie-ID
 		return categoryId;
 	}
 	
-	public byte getBuildId() {
+	public byte getBuildId() {		// Rueckgabe der Gebaeude-ID
 		return buildingId;
 	}
 	
